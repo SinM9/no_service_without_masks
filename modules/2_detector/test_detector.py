068ab6d98a5c00a2b35b374315d4eb49447392c0
IlyaShein <67902791+IlyaShein@users.noreply.github.com>
2020-07-13 21:34:24 +0300

    fix detector

--- modules/2_detector/test_detector.py
+++ modules/2_detector/test_detector.py
@@ -1,27 +1,20 @@
+import os
+import detector
 import numpy as np
 import cv2 as cv
 import unittest
-from detector import nms
-from detector import iou
+from detector import detect
 
 class TestForNMSandIOU(unittest.TestCase):
-    def test_nms(self):
-        boxes = [ [24, 17, 99, 146], [38, 38, 126, 172], [30, 88, 180, 193] ]
-        probs = [0.81, 0.89, 0.92]
-        indices = []
     
-        nms(boxes, probs, 0.4, indices)
-
-        self.assertEqual(len(indices), 2)
-
-    def test_iou(self):
-
-        a = [21, 27, 131, 177]
-        b = [53, 56, 161, 208]
-
-        self.assertLessEqual((iou(a, b) - 0.401993), 1e-5)
-
-
+    def test_detect(self):
+        data_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
+        img_file=os.path.join(data_folder,'conference.png')
+        box=detect(data_folder,img_file)
+        k=0
+        for i in box:
+           k=k+1
+        self.assertEqual(k,29)
 if __name__ == '__main__':
     unittest.main()
 
