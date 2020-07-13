a72ae75d33d7d49d4d5754d7fc039ffd01fe2e04
SinM9 <marusya19.09.99@gmail.com>
2020-07-13 04:37:56 +0300

    okay. now there should definitely be no errors

--- modules/1_sourse/test_sourse.py
+++ modules/1_sourse/test_sourse.py
@@ -1,18 +1,18 @@
 import unittest
-import sourse
+import calc
  
 class CalcTest(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(sourse.add(1, 2), 3)
+        self.assertEqual(calc.add(1, 2), 3)
         
     def test_sub(self):
-        self.assertEqual(sourse.sub(4, 2), 2)
+        self.assertEqual(calc.sub(4, 2), 2)
         
     def test_mul(self):
-        self.assertEqual(sourse.mul(2, 5), 10)
+        self.assertEqual(calc.mul(2, 5), 10)
         
     def test_div(self):
-        self.assertEqual(sourse.div(8, 4), 2)
+        self.assertEqual(calc.div(8, 4), 2)
         
 if __name__ == '__main__':
     unittest.main()
