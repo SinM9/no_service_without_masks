36c38cc8b17a0976b067b9cd88653bca925790ff
Sinitsina Maria <49319156+SinM9@users.noreply.github.com>
2020-07-23 17:41:49 +0300

    Revert "fix"

---
+++ modules/0_first_try/src/sourse.cpp
@@ -0,0 +1,11 @@
+#include "header.hpp"
+
+using namespace cv;
+
+int FunctionName(int k){
+	int privet = 0;
+	if (k > 5)
+		return (privet);
+	else 
+		return (1);
+}
---
+++ modules/1_video_connection/main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+from detector import detect
+from metric import calc_metric
+
+cap = cv2.VideoCapture('test.mp4')
+
+threshold = 0.37
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    boxes = detect(frame)
+
+    for box in boxes:
+        img = frame[box[1]:box[3], box[0]:box[2]]
+
+        metric = calc_metric(img)
+
+        cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (232, 35, 244), 2)
+
+        if (metric > threshold):
+            cv2.putText(frame, 'mask', (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX,  
+                   1, (0, 255, 0) , 2, cv2.LINE_AA) 
+        else:
+            cv2.putText(frame, 'no mask', (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX,  
+                   1, (0, 0, 255) , 2, cv2.LINE_AA) 
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
