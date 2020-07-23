2ed3c4521442d11535fae28468ea32d096e24c2c
yasakova-anastasia <yasakova_anastasiya@mail.ru>
2020-07-23 17:36:14 +0300

    fix

--- modules/0_first_try/include/header.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <opencv2/opencv.hpp>
-#include <inference_engine.hpp> //думаю нужна для детектирования лиц
-
-// объявления  функций, классов  + объявления нужных директорий openVino
-int FunctionName(int k);
